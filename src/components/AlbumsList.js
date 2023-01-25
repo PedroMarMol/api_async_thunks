33253f5d2afa67bdfba3199ed74b2347b485ead0
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-25 20:14:05 +0100

    remove implementation

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,6 @@
 import { 
   useFetchAlbumsQuery, 
-  useAddAlbumMutation,
-  useRemoveAlbumMutation 
+  useAddAlbumMutation
 } from '../store';
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
@@ -11,7 +10,6 @@ import AlbumsListItem from './AlbumsListItem';
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
-  const [removeAlbum, removeAlbumResults] = useRemoveAlbumMutation();
 
   const handleAddAlbum = () => {
     addAlbum(user);
